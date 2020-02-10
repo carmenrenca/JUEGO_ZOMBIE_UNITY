794e6117e777a5d5c3a45b3476bcc99a6c1ae34d
carmen <carmen@carmenweb.com>
2020-02-10 22:38:28 +0100

    commit juego pc

---
+++ Assets/script/VolverAJugar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement ;
+public class VolverAJugar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    Cursor.visible=false;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        if (Input.GetKey(KeyCode.E)){
+             Application.Quit();
+        }
+
+        if(Input.GetKey(KeyCode.I)){
+            EmpezarJuego();
+        }
+        
+    }
+
+    public void EmpezarJuego(){
+
+  SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void salir(){
+        Application.Quit();
+    }
+}
---
+++ Assets/script/cofretesoro.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+using System.Collections.Generic;
+using System.Threading;
+public class cofretesoro : MonoBehaviour
+{
+
+int sizeList;
+   public Animator animadorganar;
+  protected AudioSource audiosource;
+    public AudioClip Sonido;
+    public GameObject cofre;
+      public GameObject panel;
+    private NavMeshAgent agente;
+    public Puntuacion puntuacion;
+   // public GameObject[] posiciones;
+public List<GameObject> posiciones=new List<GameObject>();
+    public Collider m_ObjectCollider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        generarcofre();
+
+        audiosource = GetComponent<AudioSource>();
+        agente = GetComponent<NavMeshAgent>();
+        m_ObjectCollider = GetComponent<Collider>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+if (Input.GetKey(KeyCode.F10)){
+            salir();
+        }
+
+        if(Input.GetKey(KeyCode.I)){
+ReiniciarJuego();     }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        audiosource.PlayOneShot(Sonido);
+        Destroy(this.gameObject);
+         
+  animadorganar.SetTrigger("Mostrar"); 
+panel.SetActive(false);
+
+     if (Input.GetKey(KeyCode.E)){
+             Application.Quit();
+        }
+
+        if(Input.GetKey(KeyCode.I)){
+           ReiniciarJuego();
+        }
+        
+
+    }
+
+
+    public void salir(){
+        Application.Quit();
+    }
+    void generarcofre()
+    {
+    int aux=0 ;
+        sizeList=posiciones.Count;
+     
+     do{
+
+    
+        if(sizeList==0){
+            ReiniciarJuego();
+        }
+           float randomNumber = Random.Range(0, sizeList);
+        for (int i = 0; i < sizeList; i++)
+        {
+
+
+            if (i == randomNumber)
+            {
+             
+                if (posiciones[i] != null)
+                {
+                    cofre.transform.position = posiciones[i].transform.position;
+                    posiciones.RemoveAt(i);
+
+                }else{
+                    aux=1;
+                }
+
+
+
+
+            }
+        }
+
+         }while(aux==1);
+    }
+
+
+    void ReiniciarJuego()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        AudioListener.volume = 1f;
+
+
+    }
+}
